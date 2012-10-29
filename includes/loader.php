551acfe2592a32aeb1c09a1a6bd5bd4d77212316
spurge <olof@klandestino.se>
2012-10-29 08:31:28 +0100

    Added readme and som comments. Changed the component extension to group extension

--- includes/loader.php
+++ includes/loader.php
@@ -1,24 +1,41 @@
 <?php
 
+/**
+ * If not ABSPATH is defined, then this plugin is not loaded into wordpress.
+ * Then exit.
+ */
 if( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-class BP_Relate_Groups_to_Blogs extends BP_Component {
+/**
+ * Plugin/component witch is an extension of the Group Components
+ */
+class BP_Relate_Groups_to_Blogs extends BP_Group_Extension {
 
+	/**
+	 * The contructor
+	 * Starts the plugin and hooks some actions
+	 */
 	function __construct() {
 		parent::start(
 			'bp-relate-group-to-blogs',
 			__( 'BP Relate Groups to Blogs', 'bp-relate-groups-blogs' ),
 			BP_RELATE_GROUPS_TO_BLOGS_PLUGIN_DIR
 		);
+
+		//
 	}
 
 }
 
+/**
+ * Used with the bp_loaded action to load this plugin
+ */
 function bp_relate_groups_to_blogs_load_component() {
 	global $bp;
 	$bp->relate_groups_to_blogs = new BP_Relate_Groups_to_Blogs();
 }
 
+// Hook the bp_loaded action
 add_action( 'bp_loaded', 'bp_relate_groups_to_blogs_load_component' );
---
+++ readme.txt
@@ -0,0 +1,22 @@
+=== Plugin Name ===
+Contributors: spurge, lakrisgubben, alfreddatakillen
+Tags: buddypress, groups, blogs
+Requires at least: WordPress 3.4.2, BuddyPress 1.6.1
+Tested up to: WordPress 3.4.2 / BuddyPress 1.6.1
+Stable tag: 0.9
+
+Makes it possible to relate groups to blogs.
+
+== Description ==
+
+When groups are started or edited, you can relate them to several blogs.
+These relationships will then be displayed on both groups and blogs.
+
+== Installation ==
+
+Download and upload the plugin to your plugins folder. Then activate it
+in your network administration.
+
+== Notes ==
+
+License.txt - contains the licensing details for this component.
